b2cf3a485d9f9e202c8f0b9f749b6741657e1754
takumi <konnitiha@outlook.com>
2022-08-10 10:01:58 +0900

    add

---
+++ train_note/links.md
@@ -0,0 +1,2 @@
+### 20220810
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/konnitiha3/MOD2NN/blob/master/train_note/20220810.ipynb)
---
+++ train_note/make_links.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import re
+
+files = glob.glob("*")
+files.sort()
+notes = []
+for f in files:
+    if re.match(r'.*ipynb', f):
+        notes.append(f)
+
+md = ""
+for note in notes:
+    md = md + "### " + os.path.splitext(note)[0] + "\n"
+    md = md + "[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/konnitiha3/MOD2NN/blob/master/train_note/" + note + ")\n"
+
+with open("links.md", mode="w") as f:
+    f.write(md)
